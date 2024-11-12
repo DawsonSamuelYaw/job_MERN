65ee39bf2473dd784e95a115a27c17b1659266ef
Dawson Samuel <dawsonsam05595@gmail.com>
2024-11-12 21:11:33 +0000

    job style

--- Frontend/src/components/job.jsx
+++ Frontend/src/components/job.jsx
@@ -5,21 +5,21 @@ import Jobs from '../jobs.json';
 const Job = () => {
   return (
     <>
-      <div className="container mx-auto p-6">
+      <div className="container mx-auto p-6 font-[poppins]">
         <h1 className="text-3xl font-bold text-center mb-6 text-[#2148C0]">Available Jobs</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {Jobs.map((item) => (
             <div
-              className="sub bg-white shadow-lg rounded-lg p-6 border-t-4 border-[#2148C0] transform transition duration-300 hover:scale-105"
+              className="sub bg-white shadow-lg rounded-lg p-6 border-t-2 border-[#2148C0] transform transition duration-300 hover:scale-105"
               key={item.id}
             >
               <h2 className="text-lg font-semibold text-gray-800 mb-2">{item.title}</h2>
               <p className="text-sm text-gray-600 mb-4">{item.type}</p>
               <p className="text-gray-700 mb-4">{item.description}</p>
               <p className="text-gray-500 mb-4">{item.location}</p>
-              <h3 className="text-md font-semibold text-gray-800 mb-4">${item.salary}</h3>
+              <h3 className="text-md font-semibold text-red-600 mb-4">{item.salary}</h3>
               <div className="text-center">
-                <button className="bg-[#2148C0] text-white px-4 py-2 rounded-lg hover:bg-[#1b3a8a] transition duration-200">
+                <button className="bg-[#2148C0] text-white w-full py-2 rounded-lg hover:bg-transparent hover:text-[#2148C0] transition duration-200 hover:border hover:border-[#2148C0]">
                   Read More..
                 </button> 
               </div>
